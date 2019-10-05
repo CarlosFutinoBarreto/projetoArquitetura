1c7d62fd4912542544f923f30bc692fb2090c761
Gustavo Vieira <guga@Gustavos-MacBook-Pro.local>
2019-10-05 08:20:49 -0300

    Updated: keyboardManager

--- src/gui/misc/KeyboarManager.java
+++ src/gui/misc/KeyboarManager.java
@@ -4,35 +4,32 @@ import java.awt.KeyEventDispatcher;
 import java.awt.KeyboardFocusManager;
 import java.awt.event.KeyEvent;
 
-import gameElements.GameCharacter;
 import gui.GamePanel;
 
 public class KeyboarManager {
-	
-	private static final int KEY_LEFT=37;
-	private static final int KEY_UP=38;
-	private static final int KEY_RIGHT=39;
-	private static final int KEY_DOWN=40;
-	private static final int KEY_SPACE=32;
 
 	public static void setKeys(GamePanel gp) {
-		
+
 		KeyboardFocusManager.getCurrentKeyboardFocusManager()
-		  .addKeyEventDispatcher(new KeyEventDispatcher() {
-		      @Override
-		      public boolean dispatchKeyEvent(KeyEvent e) {
-		        if (e.getID()==KeyEvent.KEY_PRESSED) {
-		        		switch(e.getKeyCode()) {
-		        		case KEY_UP:gp.walk(GameCharacter.UP);break;
-		        		case KEY_DOWN:gp.walk(GameCharacter.DOWN);break;
-		        		case KEY_LEFT:gp.walk(GameCharacter.LEFT);break;
-		        		case KEY_RIGHT:gp.walk(GameCharacter.RIGHT);break;
-		        		case KEY_SPACE:gp.shoot();break;
-		        		}
-		        	}
-		        return false;
-		      }
-		});
+				.addKeyEventDispatcher(new KeyEventDispatcher() {
+					@Override
+					public boolean dispatchKeyEvent(KeyEvent e) {
+						if (e.getID()==KeyEvent.KEY_PRESSED) {
+							if (e.getKeyCode() == gp.getUp().getKey()) {
+								gp.getUp().execute(gp);
+							}else if (e.getKeyCode() == gp.getDown().getKey()) {
+								gp.getDown().execute(gp);
+							}else if (e.getKeyCode() == gp.getRight().getKey()) {
+								gp.getRight().execute(gp);
+							}else if (e.getKeyCode() == gp.getLeft().getKey()) {
+								gp.getLeft().execute(gp);
+							}
+						}else if (e.getKeyCode() == gp.getShoot().getKey()) {
+							gp.getShoot().execute(gp);
+						}
+						return false;
+					}
+				});
 	}
-	
+
 }
